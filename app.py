4b60db43ca1ed908b5290366afd9c0ae050e3381
Danny <rock86109@gmail.com>
2022-01-12 18:21:12 +0800

    first

--- app.py
+++ app.py
@@ -265,14 +265,14 @@ def getImageMessage(originalContentUrl):
 def replyMessage(payload):
     url = "https://api.line.me/v2/bot/message/reply"
 
-    response = request.post(url)
+    response = request.post(url, header = HEADER)
     print(response.text)
     return response.text
 
 
 def pushMessage(payload):
     url = "https://api.line.me/v2/bot/message/push"
-    response = {}
+    response = request.post(url, header = HEADER)
     print(response.text)
     return response.text
 
