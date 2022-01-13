c3f1cc77db660d2973477d03acdf9ee200c4bca3
Danny <rock86109@gmail.com>
2022-01-13 18:23:02 +0800

    first

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def index():
                 replyMessage(payload)
             else:
                 form_data = events[0]["postback"]["data"]
-                data = json.dumps({form_data.split("=")[0], form_data.split("=")[1]})
+                data = json.loads({form_data.split("=")[0], form_data.split("=")[1]})
                 
                 # data = json.loads(events[0]["postback"]["data"])
                 action = data["action"]
